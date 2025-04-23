cd2f4e216f2e9466dd60183647380d653a98c9bd
ArthurDesc <arthur.descourvieres@gmail.com>
2025-04-23 21:45:00 +0200

    Add user seeder for Arthur Descourvieres with sample credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Arthur Descourvieres',
+            'email' => 'arthur.descourvieres@gmail.com',
+            'password' => bcrypt('Password123@@'),
+        ]);
     }
 }
