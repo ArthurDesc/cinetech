3dace8e8de36d3b92e3305325d9dc011b161f7d3
ArthurDesc <arthur.descourvieres@gmail.com>
2025-05-02 16:06:32 +0200

    Add HTTPS redirection and CORS headers; improve error handling and disable directory listing

--- public/index.php
+++ public/index.php
@@ -5,51 +5,29 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$coreDirectory = realpath(__DIR__.'/../../cinetech_core');
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+try {
+    if (!is_readable($coreDirectory.'/vendor/autoload.php')) {
+        die('Impossible de lire autoload.php');
+    }
+    
+    if (!is_readable($coreDirectory.'/bootstrap/app.php')) {
+        die('Impossible de lire app.php');
+    }
+    
+    require $coreDirectory.'/vendor/autoload.php';
+    
+    $app = require_once $coreDirectory.'/bootstrap/app.php';
+    
+    $kernel = $app->make(Kernel::class);
+    
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+    
+    $kernel->terminate($request, $response);
+    
+} catch (Throwable $e) {
+    throw $e;
 }
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
